8636f2d88a961fb251a4ad4a720d4a0b7ebf6e78
manswirfelt <mans.wirfelt@gmail.com>
2023-06-27 17:33:57 +0200

    Peer graded assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions for increases efficiency when calculating inverses 
+## of many 2x2 matrices. If an inverse has already been calculated before
+## we get the cached version instead of calculating it again
 
-## Write a short comment describing this function
+## The function below takes a matrix as an argument and returns a list of 
+## functions 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function below takes in a matrix and checks whether the inverse of that
+## matrix have been calculated previously or not. 
+## If it has been calculated previously, it returns the old inverse that is 
+## being grabbed by x$getinverse()
+## If not previously calculated, the Sovle() function is used to calculate it 
+## and the inverse matrix gets set in x and then returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m   ## Return a matrix that is the inverse of 'x'
 }
